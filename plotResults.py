647bd592eb4b72bbba6e3cad613dc8fff247af4d
trinidadb <triniburs1@gmail.com>
2024-03-11 01:13:38 +0100

    Almost done

--- plotResults.py
+++ plotResults.py
@@ -7,7 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 resultsAll = EvaluatorAll()
-resultsByQuery = EvaluatorByQuery(firstSortByQuery=True)
+resultsByQueryId = EvaluatorByQuery(firstSortByQuery=True)
 resultsByRunId = EvaluatorByQuery(firstSortByQuery=False)
 
 class Plot():
@@ -17,16 +17,18 @@ class Plot():
 
     def showTable(self):
         otherMetrics = self.metrics.drop(columns=[col for col in self.metrics.columns if col.startswith(('P_', 'iprec_'))])
+        otherMetrics.to_csv("output.csv")
         print(otherMetrics)
 
-    def plotExtended(self, byRunId=False, iprecCurve=True):
+    def plotExtended(self, byRunId=False, iprecCurve=False):
         key = "runId" if byRunId else "queryId"
         ids = self.metrics.index.get_level_values(key).unique()
 
         nRows = 2 if byRunId else 5
         nCols = 2 if byRunId else 5
+        constrained_layout = False if byRunId else True
 
-        fig, axs = plt.subplots(nRows, nCols, figsize=(50, 50), constrained_layout=True)
+        fig, axs = plt.subplots(nRows, nCols, figsize=(50, 50), constrained_layout=constrained_layout)
         axs = axs.flatten()
 
         for i, id in enumerate(ids):
@@ -40,24 +42,44 @@ class Plot():
             axs[i].get_legend().remove()
 
         fig.legend(handles, labels, loc='center right', title="QueryId" if byRunId else "RunId", fontsize=9)
-        fig.supxlabel("Recall")
-        fig.supylabel("IPrecision")
-        fig.suptitle(f"Interpolated Precison VS Recall Curve {'By Index' if byRunId else 'By Query'}")
+        
+        if iprecCurve:
+            fig.supxlabel("Recall")
+            fig.supylabel("IPrecision")
+            fig.suptitle(f"Interpolated Precison VS Recall Curve {'By Query' if byRunId else 'By Index'}")
+        else:
+            fig.supxlabel("Number of Documents")
+            fig.supylabel("Precision")
+            fig.suptitle(f"Precision at different cut-off points {'By Query' if byRunId else 'By Index'}")
 
         plt.show()
 
-    def plotRPrecision(self):
-        precisionTable = self.metrics.filter(regex='^P_')
+    def plotRPrecision(self, metrics=None, id="all", customAx=None):
+        if metrics is None:
+            metrics = self.metrics
+        precisionTable = metrics.filter(regex='^P_')
         precisionTable = precisionTable.rename(columns={'P_5': 5, 'P_10': 10, 'P_15': 15, 'P_20': 20, 'P_30': 30, 'P_100': 100, 'P_200': 200, 'P_500': 500, 'P_1000': 1000})
         precisionTable.sort_index(axis=1, inplace=True)
         precisionTable = precisionTable.astype(float)
 
-        # transpose and plot
-        ax = precisionTable.T.plot(figsize=(7, 6))
-        ax.set_ylabel('Precision', fontsize=12)
-        ax.set_xlabel('Number of Documents', fontsize=12)
-        ax.set_title("Precision at different cut-off points")
-        plt.show()
+        custom_colors = ['blue', 'green', 'red', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan',
+                'navy', 'lime', 'maroon', 'gold', 'indigo', 'sienna', 'magenta', 'dimgray', 'olivedrab', 'darkcyan',
+                'lightsteelblue', 'limegreen', 'tomato', 'khaki', 'violet']
+        
+        ax = customAx if customAx else plt.gca()
+        precisionTable.T.plot(ax=ax, figsize=(7, 6), color=custom_colors)
+
+        #mplcursors.cursor(hover=True).connect("add", lambda sel: sel.annotation.set_text(sel.artist.get_label()))
+
+        if customAx:
+            ax.set_xlabel('')
+            ax.set_title(f"{id}", fontsize=10)
+        else:
+            ax.set_ylabel('Precision', fontsize=12)
+            ax.set_xlabel('Number of Documents', fontsize=12)
+            ax.set_title("Precision at different cut-off points")
+            plt.legend(title=metrics.index.names[0], fontsize=7)
+            plt.show()
 
     def plotIPrecAtRecall(self, metrics=None, id="all", customAx=None):
         if metrics is None:
@@ -72,11 +94,10 @@ class Plot():
                         'navy', 'lime', 'maroon', 'gold', 'indigo', 'sienna', 'magenta', 'dimgray', 'olivedrab', 'darkcyan',
                         'lightsteelblue', 'limegreen', 'tomato', 'khaki', 'violet']
 
-        # Transpose and plot with custom color palette
         ax = customAx if customAx else plt.gca()
         iPrecisionTable.T.plot(ax=ax, figsize=(7, 6), color=custom_colors)
 
-        mplcursors.cursor(hover=True).connect("add", lambda sel: sel.annotation.set_text(sel.artist.get_label()))
+        #mplcursors.cursor(hover=True).connect("add", lambda sel: sel.annotation.set_text(sel.artist.get_label()))
 
         if customAx:
             ax.set_xlabel('')
@@ -91,5 +112,11 @@ class Plot():
 
 
 #Plot(resultsAll.results).plotIPrecAtRecall()
+#Plot(resultsAll.results).plotRPrecision()
+#Plot(resultsByQueryId.results).plotExtended(False, False)
+#Plot(resultsByQueryId.results).plotExtended(False, True)
+#Plot(resultsByRunId.results).plotExtended(True, False)
+#Plot(resultsByRunId.results).plotExtended(True, True)
         
-Plot(resultsByRunId.results).plotExtended()
+#Plot(resultsByQueryId.results).showTable()
+Plot(resultsByRunId.results).showTable()
